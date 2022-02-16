86e837b7a7a76c59696998f08dcc3c89222afda4
Jason Kuan <jsonkuan@icloud.com>
2022-02-16 09:08:41 +0100

    Create helper method foor test readability

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -30,7 +30,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
 
-        expect(sut: sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.connectivity)) {
+        expect(sut: sut, toCompleteWithResult: failure(.connectivity)) {
             let clientError = NSError(domain: "test", code: 0, userInfo: nil)
             client.complete(with: clientError, at: 0)
         }
@@ -41,7 +41,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
         let samples = [199, 201, 400, 500]
         samples.enumerated().forEach { index, code in
-            expect(sut: sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect(sut: sut, toCompleteWithResult: failure(.invalidData)) {
                 let json = makeItemsJSON([] )
                 client.complete(statusCode: code, data: json, at: index)
             }
@@ -51,7 +51,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
 
-        expect(sut: sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut: sut, toCompleteWithResult: failure(.invalidData)) {
             let invalidJSON = Data("Invalid json".utf8)
             client.complete(statusCode: 200, data: invalidJSON)
         }
@@ -105,6 +105,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
         trackForMemoryLeak(client)
 
         return (sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        .failure(error)
     }
 
     private func trackForMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
