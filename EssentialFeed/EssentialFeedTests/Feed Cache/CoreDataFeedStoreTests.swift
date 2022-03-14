b39778c175cfd46add71ade4a7c827141fd73eef
Jason Kuan <jsonkuan@icloud.com>
2022-03-14 21:43:39 +0100

    Reuse shared test assertions on 'CoreDataFeedStoreTests'

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -22,48 +22,68 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
-        
+
         assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
 
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        let sut = makeSUT()
 
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
 
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        let sut = makeSUT()
 
+        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
 
     func test_insert_deliversErrorOnInsertionError() {
+        let sut = makeSUT()
 
+        assertThatInsertDeliversErrorOnInsertionError(on: sut)
     }
 
     func test_insert_hasNoSideEffectsOnInsertionError() {
+        let sut = makeSUT()
 
+        assertThatInsertHasNoSideEffectsOnInsertionError(on: sut)
     }
 
     func test_insert_overridesPreviouslyInsertedCacheValues() {
+        let sut = makeSUT()
 
+        assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
     }
 
     func test_delete_deliversErrorOnDeletionError() {
+        let sut = makeSUT()
 
+        assertThatDeleteDeliversErrorOnDeletionError(on: sut)
     }
 
     func test_delete_hasNoSideEffectsOnDeletionError() {
+        let sut = makeSUT()
 
+        assertThatDeleteHasNoSideEffectsOnDeletionError(on: sut)
     }
 
     func test_delete_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
 
+        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
     }
 
     func test_delete_emptiesPreviouslyInsertedCache() {
+        let sut = makeSUT()
 
+        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
     }
 
     func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
 
+        assertThatStoreSideEffectsRunSerially(on: sut)
     }
 
     // MARK: - Helpers
