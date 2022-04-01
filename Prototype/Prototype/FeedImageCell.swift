2378e426f604addedfbe912b381660f21600f07d
Jason Kuan <jsonkuan@icloud.com>
2022-04-01 13:25:40 +0200

    Simulate loading animation with fade

--- Prototype/Prototype/FeedImageCell.swift
+++ Prototype/Prototype/FeedImageCell.swift
@@ -5,5 +5,28 @@ final class FeedImageCell: UITableViewCell {
     @IBOutlet private(set) var descriptionLabel: UILabel!
     @IBOutlet private(set) var locationContainer: UIView!
     @IBOutlet private(set) var feedImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        feedImageView.alpha = 0
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        feedImageView.alpha = 0
+    }
+    
+    func fadeIn(_ image: UIImage?) {
+        feedImageView.image = image
+        
+        UIView.animate(withDuration: 0.3,
+                       delay: 0.3,
+                       options: [],
+                       animations: {
+            self.feedImageView.alpha = 1
+        })
+    }
 }
 
