c2937baf34402853fd7a05225dcc34a18b9d0264
Jason Kuan <jsonkuan@icloud.com>
2022-04-02 11:00:24 +0200

    Replace spy with 'FeedLoader'

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -1,9 +1,10 @@
 import XCTest
+import EssentialFeed
 
 class FeedViewController: UIViewController {
-    private var loader: FeedViewControllerTests.LoaderSpy?
+    private var loader: FeedLoader?
 
-    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+    convenience init(loader: FeedLoader) {
         self.init()
 
         self.loader = loader
@@ -12,7 +13,7 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        loader?.load()
+        loader?.load() { _ in }
     }
 }
 
@@ -35,10 +36,10 @@ final class FeedViewControllerTests: XCTestCase {
 
     // MARK: - Helpers
 
-    final class LoaderSpy {
+    final class LoaderSpy: FeedLoader {
         private (set) var loadCallCount = 0
 
-        func load() {
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
             loadCallCount += 1
         }
     }
