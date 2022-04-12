906c06b796c1d61a299d3a09e4f331896c23b4ea
Jason Kuan <jsonkuan@icloud.com>
2022-04-12 13:12:44 +0200

    Swap property injection to constructor injection
    Guarantee's the 'FeedPresenter' has its required properties,
    to prevent leaking composition details.

--- EssentialFeed/EssentialFeediOS/Feed UI/Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Presentation/FeedPresenter.swift
@@ -16,19 +16,24 @@ protocol FeedView {
 }
 
 final class FeedPresenter {
-    var loadingView: FeedLoadingView?
-    var feedView: FeedView?
-
+    private let loadingView: FeedLoadingView
+    private let feedView: FeedView
+    
+    init(loadingView: FeedLoadingView, feedView: FeedView) {
+        self.loadingView = loadingView
+        self.feedView = feedView
+    }
+    
     func didStartLoadingFeed() {
-        loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: true))
+        loadingView.display(viewModel: FeedLoadingViewModel(isLoading: true))
     }
     
     func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView?.display(viewModel: FeedViewViewModel(feed: feed))
-        loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: false))
+        feedView.display(viewModel: FeedViewViewModel(feed: feed))
+        loadingView.display(viewModel: FeedLoadingViewModel(isLoading: false))
     }
     
     func didFinishLoadingFeed(with error: Error) {
-        loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: false))
+        loadingView.display(viewModel: FeedLoadingViewModel(isLoading: false))
     }
 }
