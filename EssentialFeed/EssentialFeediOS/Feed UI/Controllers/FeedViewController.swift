d6bebff1c909a5369b0894c558d217ea1915d23e
Jason Kuan <jsonkuan@icloud.com>
2022-05-13 10:24:55 +0200

    feature/feed error ui (#8)
    * Add feed error localizations
    
    * Does not render feed error view on load
    
    * Render error message on load feed error
    
    * Hide feed load error on reload
    
    * Move 'ErrorView' to storyboard
    
    * Move 'ErrorView' to separate file
    
    * Configure ErrorView layout in storyboard
    
    * Show and hide error message animated
    
    * Hide error messsage on tap
    
    * Move view helpers to new group
    
    * Extract refresh logic to extension
    
    * Add factory helpers to clarify intent of displaying or hiding error

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -4,15 +4,17 @@ protocol FeedViewControllerDelegate {
     func didRequestFeedRefresh()
 }
 
-public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
-   var delegate: FeedViewControllerDelegate?
-
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
+    @IBOutlet private(set) public var errorView: ErrorView?
+    
+    var delegate: FeedViewControllerDelegate?
+    
     var tableModel = [FeedImageCellController]() {
         didSet {
             tableView.reloadData()
         }
     }
-
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,41 +24,41 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     @IBAction private func refresh() {
         delegate?.didRequestFeedRefresh()
     }
- 
+    
     func display(_ viewModel: FeedLoadingViewModel) {
-        if viewModel.isLoading {
-            refreshControl?.beginRefreshing()
-        } else {
-            refreshControl?.endRefreshing()
-        }
+        refreshControl?.update(isRefreshing: viewModel.isLoading)
     }
-
+    
+    func display(_ viewModel: FeedErrorViewModel) {
+        errorView?.message = viewModel.message
+    }
+    
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         tableModel.count
     }
-
+    
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         cellControllerForRow(at: indexPath).view(in: tableView)
     }
-
+    
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cancelCellControllerLoads(forRowAt: indexPath)
     }
-
+    
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
             cellControllerForRow(at: indexPath).preload()
         }
     }
-
+    
     public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach(cancelCellControllerLoads)
     }
-
+    
     private func cellControllerForRow(at indexPath: IndexPath) -> FeedImageCellController {
         tableModel[indexPath.row]
     }
-
+    
     private func cancelCellControllerLoads(forRowAt index: IndexPath) {
         cellControllerForRow(at: index).cancelLoad()
     }
