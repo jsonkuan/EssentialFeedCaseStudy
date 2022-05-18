803781ff3b8e69c6e179a2c3ddc6e71932aee404
Jason Kuan <jsonkuan@icloud.com>
2022-05-18 11:05:53 +0200

    'FeedImagePresenter' displays error on failed image transformation

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -29,10 +29,13 @@ protocol FeedImageView {
 }
 
 final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private struct InvalidImageDataError: Error {}
     private let view: View
+    private let imageTransformer: (Data) -> Image?
 
-    init(view: View) {
+    init(view: View, imageTransformer: @escaping (Data) -> Image?) {
         self.view = view
+        self.imageTransformer = imageTransformer
     }
 
     func didStartLoadingImageData(for model: FeedImage) {
@@ -41,6 +44,28 @@ final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == I
             location: model.location,
             image: nil,
             isLoading: true,
+            shouldRetry: false))
+    }
+    
+    func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: false,
+            shouldRetry: true))
+    }
+    
+    func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+        guard let image = imageTransformer(data) else {
+            return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
+        }
+
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: image,
+            isLoading: false,
             shouldRetry: false))
     }
 }
@@ -61,11 +86,37 @@ final class FeedImagePresenterTests: XCTestCase {
         XCTAssertEqual(view.messages, [.display(.loadingViewModel(feedImage: feedImage))])
     }
     
+    func test_didFinishLoadingImageDataWithError_displaysErrorViewModel() {
+        let (sut, view) = makeSUT()
+        let error = anyNSError()
+        let image = uniqueFeedImage()
+        
+        sut.didFinishLoadingImageData(with: error, for: image)
+        
+        XCTAssertEqual(view.messages, [.display(.errorViewModel(feedImage: image))])
+    }
+    
+    func test_didFinishLoadingImageData_displaysErrorViewModelWithFailedImageTransformation() {
+        let (sut, view) = makeSUT()
+        let image = uniqueFeedImage()
+        let imageStub = NSImage.make()
+        let data = imageStub.tiffRepresentation!
+    
+        sut.didFinishLoadingImageData(with: data, for: image)
+        
+        XCTAssertEqual(view.messages, [
+            .display(.errorViewModel(feedImage: image))
+        ])
+    }
+    
     // MARK: -  Helpers
 
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (FeedImagePresenter<ViewSpy, FeedImage>, ViewSpy) {
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (FeedImagePresenter<ViewSpy, NSImage>, ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedImagePresenter(view: view)
+        let sut = FeedImagePresenter(
+            view: view,
+            imageTransformer: NSImage.init(data:)
+        )
         
         trackForMemoryLeak(view)
         trackForMemoryLeak(sut)
@@ -74,7 +125,7 @@ final class FeedImagePresenterTests: XCTestCase {
     }
     
     private class ViewSpy: FeedImageView {
-        typealias Image = FeedImage
+        typealias Image = NSImage
         
         enum Messages: Equatable {
             case display(_ viewModel: FeedImageViewModel<Image>)
@@ -97,5 +148,31 @@ private extension FeedImageViewModel {
             isLoading: true,
             shouldRetry: false
         )
+    }
+    
+    static func errorViewModel(feedImage: FeedImage) -> FeedImageViewModel {
+        FeedImageViewModel(
+            description: feedImage.description,
+            location: feedImage.location,
+            image: nil,
+            isLoading: false,
+            shouldRetry: true
+        )
+    }
+    
+    static func successViewModel(data: Data, feedImage: FeedImage) -> FeedImageViewModel {
+        FeedImageViewModel(
+            description: feedImage.description,
+            location: feedImage.location,
+            image: NSImage.init(data: data) as? Image,
+            isLoading: false,
+            shouldRetry: false
+        )
+    }
+}
+
+extension NSImage {
+    static func make() -> NSImage {
+        NSImage(size: NSSize(width: 1, height: 1))
     }
 }
